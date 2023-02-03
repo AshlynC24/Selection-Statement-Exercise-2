dc7db89817b636e54aeeaaa72c2cf0c3cefea616
Ashlyn Carmosino <ashlyncarmosino@Ashlyns-Air.lan>
2023-02-03 14:26:01 -0500

    exercise 2 on branch 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Hello, what is your favorite subject in school?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "Math":
+                    Console.WriteLine($"Math is very interesting");
+                    break;
+                case "Science":
+                    Console.WriteLine($"That is out of this world!");
+                    break;
+                case "Spanish":
+                    Console.WriteLine($"Tu Hables Espanol");
+                    break;
+                case "English":
+                    Console.WriteLine($"Wow, you read a lot");
+                    break;
+                default:
+                    Console.WriteLine($"That must be new");
+                    break;
+
+            }
         }
     }
 }
